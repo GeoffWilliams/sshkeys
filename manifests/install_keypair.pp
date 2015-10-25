13ee51cfb1f6c8403707d2e0ab7015f0e2bf8c34
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-10-25 17:13:38 +1100

    do authrorized keys when installing keys

--- manifests/install_keypair.pp
+++ manifests/install_keypair.pp
@@ -15,6 +15,7 @@ define sshkeys::install_keypair(
   } else {
     $_ssh_dir = "/home/${user}/.ssh"
   }
+  $known_hosts          = "${_ssh_dir}/known_hosts"
 
 
   if ! defined(File[$_ssh_dir]) {
@@ -22,6 +23,14 @@ define sshkeys::install_keypair(
       ensure => directory,
     }
   }
+
+  if $name =~ /\w+@\w+/ {
+    $split_name = split($authorized_key, "@")
+    $host = $split_name[1]
+  } else {
+    fail("requested key '${name}' is not in the correct format - should be user@host")
+  }
+
 
   # private key
   file { "${_ssh_dir}/${name}":
@@ -33,6 +42,12 @@ define sshkeys::install_keypair(
   file { "${_ssh_dir}/${name}.pub":
     ensure  => file,
     content => file("${source}.pub"),
+  }
+
+  exec { "known_host_${user}_${host}":
+    user    => $user,
+    command => "/usr/bin/ssh-keyscan -H ${host} >> $known_hosts",
+    unless  => "/usr/bin/ssh-keygen -F ${host}",
   }
 
 }
