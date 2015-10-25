86a4b1840aa45250384f6f02f67df1c9863bcab1
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-10-25 17:57:14 +1100

    fixup homedir for ssh

--- manifests/known_host.pp
+++ manifests/known_host.pp
@@ -2,19 +2,17 @@ define sshkeys::known_host(
   $ssh_dir = false,
 ) {
 
-  if $ssh_dir {
-    $_ssh_dir = $ssh_dir
-  } else {
-    $_ssh_dir = "/home/${title}/.ssh"
-  }
-  $known_hosts          = "${_ssh_dir}/known_hosts"
-
   if $title =~ /\w+@\w+/ {
    
     $split_name = split($title, "@")
     $user = $split_name[0]
     $host = $split_name[1]
-
+    if $ssh_dir {
+      $_ssh_dir = $ssh_dir
+    } else {
+      $_ssh_dir = "/home/${user}/.ssh"
+    }
+    $known_hosts          = "${_ssh_dir}/known_hosts"
 
     exec { "known_host_${user}_${host}":
       user    => $name,
